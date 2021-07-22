12120bf1aac600c495a99049708f4cca48529867
Laura <lnkeat@gmail.com>
2021-07-21 21:15:47 -0600

    commit tree 24e9f582f3cd4b92a23d2e936e0a70cf16d42f50

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+function submitData(name, email){
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name, 
+      email
+    })
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    addUsers(object);
+  })
+  .catch(function(error) {
+    addMessage(error)
+  });
+}
+
+function addUsers(usersObj){
+  let body = document.querySelector('body')
+  body.innerHTML = usersObj.id
+}
+
+
+function addMessage(error){
+  let body = document.querySelector('body')
+  body.innerHTML = error;
+  // let alert = document.createElement('p');
+  // alert.innerText = error;
+  // document.querySelector('body').appendChild(alert)
+}
+
